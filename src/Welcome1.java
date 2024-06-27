6ea6f10a2263edb93f90a6b1a6aadd07418da8d8
Firefox654 <ytspring06@gmail.com>
2024-06-26 20:51:26 -0700

    Initial Commit

---
+++ src/Welcome1.java
@@ -0,0 +1,11 @@
+
+
+public class Welcome1 {
+
+	public static void main(String[] args) {
+		// print line
+		System.out.println("Welcome to Java Programming!");
+
+	} // end method main
+
+} // end class Welcome1
---
+++ src/homework1.java
@@ -0,0 +1,20 @@
+
+public class homework1 {
+	public static void main(String[] args) {
+		// print line
+		//System.out.print("\"Trick Question\"");
+		
+		System.out.print("Delta ");
+		System.out.print("Tau");
+		System.out.print(" Chi");
+		
+		int x = -12*7+3+4/2;
+		System.out.printf("%d",  x);
+		
+		int y;
+		y = 256;
+		System.out.print(y);
+
+	} // end method main
+
+}
